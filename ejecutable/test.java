7bbf2b55468e33f78a2740f9a757e4aff934184e
TomasCv0628 <tomas2224508@correo.uis.edu.co>
2022-09-10 10:38:32 -0500

    Creación VentanaPrincipal. Caracteristicas básicas

---
+++ ejecutable/test.java
@@ -0,0 +1,11 @@
+package mvc1_mayor_3_enteros.ejecutable;
+
+import mvc1_mayor_3_enteros.vista.VentanaPrincipal;
+
+public class test 
+{
+    public static void main(String[] args) 
+    {
+        VentanaPrincipal miVentana = new VentanaPrincipal();
+    }    
+}
---
+++ vista/VentanaPrincipal.java
@@ -0,0 +1,31 @@
+package mvc1_mayor_3_enteros.vista;
+
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame
+{
+    //------------------------- 
+    // Atributos
+    //-------------------------
+
+
+    //------------------------- 
+    // Métodos
+    //-------------------------
+
+
+    // Metodo constructor
+
+    public VentanaPrincipal()
+    {
+        // Caracteristicas de la ventana
+        this.setTitle("Mayor de tres enteros");
+        this.setSize(500,500);
+        this.setLocation(100,100);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+
+
+}
